f31c8a9d7ba70cded1aaa310ab790a7a3adcaeca
yowaimono <zhangyanhai278@gmail.com>
2024-04-21 16:08:32 +0800

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module rdb-tool
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2024-04-21 15:58:13
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2024-04-21 15:58:59
+ * @FilePath: /rdb-tool/main.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package main
+
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello!")
+}
